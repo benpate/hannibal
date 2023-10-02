babea381dfb176c1c7d20cb62184b081024e259e
Ben Pate <ben@pate.org>
2023-10-02 08:59:05 -0600

    Updating "Accept" messages
    - adding `id` field makes "Accept" messages work with Pleroma.  woot!

--- pub/send_accept.go
+++ pub/send_accept.go
@@ -8,22 +8,23 @@ import (
 )
 
 // SendFollowQueueTask creates a QueueTask that sends an "Accept" message to the recipient
-func SendAcceptQueueTask(actor Actor, activity streams.Document) QueueTask {
+func SendAcceptQueueTask(actor Actor, acceptID string, activity streams.Document) QueueTask {
 	return NewQueueTask(func() error {
-		return SendAccept(actor, activity)
+		return SendAccept(actor, acceptID, activity)
 	})
 }
 
 // SendAccept sends an "Accept" message to the recipient
 // actor: The Actor that is sending the request
 // activity: The activity that has been accepted (likely a "Follow" request)
-func SendAccept(actor Actor, activity streams.Document) error {
+func SendAccept(actor Actor, acceptID string, activity streams.Document) error {
 
 	object := activity.Map()
 	delete(object, vocab.AtContext)
 
 	message := mapof.Any{
 		vocab.AtContext:      vocab.ContextTypeActivityStreams,
+		vocab.PropertyID:     acceptID,
 		vocab.PropertyType:   vocab.ActivityTypeAccept,
 		vocab.PropertyActor:  actor.ActorID,
 		vocab.PropertyObject: object,
