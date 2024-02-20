afe087318d8eee0ca7531adb9eb44bacd55b9ae0
Ben Pate <ben@pate.org>
2024-02-20 09:02:18 -0700

    Generic Router
    - Add "context" to router so that it can be used in practical applications

--- pub/router.go
+++ pub/router.go
@@ -9,19 +9,19 @@ import (
 )
 
 // Router is a simple object that routes incoming ActivityPub activities to the appropriate handler
-type Router struct {
-	routes map[string]RouteHandler
+type Router[T any] struct {
+	routes map[string]RouteHandler[T]
 }
 
 // RouteHandler is a function that handles a specific type of ActivityPub activity.
 // RouteHandlers are registered with the Router object along with the names of the activity
 // types that they correspond to.
-type RouteHandler func(activity streams.Document) error
+type RouteHandler[T any] func(context T, activity streams.Document) error
 
 // NewRouter creates a new Router object
-func NewRouter() Router {
-	return Router{
-		routes: make(map[string]RouteHandler),
+func NewRouter[T any]() Router[T] {
+	return Router[T]{
+		routes: make(map[string]RouteHandler[T]),
 	}
 }
 
@@ -36,12 +36,12 @@ func NewRouter() Router {
 // For performance reasons, this function is not thread-safe.
 // So, you should add all routes before starting the server, for
 // instance, in your app's `init` functions.
-func (router *Router) Add(activityType string, objectType string, routeHandler RouteHandler) {
+func (router *Router[T]) Add(activityType string, objectType string, routeHandler RouteHandler[T]) {
 	router.routes[activityType+"/"+objectType] = routeHandler
 }
 
 // Handle takes an ActivityPub activity and routes it to the appropriate handler
-func (router *Router) Handle(activity streams.Document) error {
+func (router *Router[T]) Handle(context T, activity streams.Document) error {
 
 	activityType := activity.Type()
 	objectType := activity.Object().Type()
@@ -61,28 +61,28 @@ func (router *Router) Handle(activity streams.Document) error {
 		if packageDebugLevel >= DebugLevelVerbose {
 			fmt.Println("HANNIBAL: Found Route: " + activityType + "/" + objectType)
 		}
-		return routeHandler(activity)
+		return routeHandler(context, activity)
 	}
 
 	if routeHandler, ok := router.routes[activityType+"/*"]; ok {
 		if packageDebugLevel >= DebugLevelVerbose {
 			fmt.Println("HANNIBAL: Found Route: " + activityType + "/*")
 		}
-		return routeHandler(activity)
+		return routeHandler(context, activity)
 	}
 
 	if routeHandler, ok := router.routes["*/"+objectType]; ok {
 		if packageDebugLevel >= DebugLevelVerbose {
 			fmt.Println("HANNIBAL: Found Route: " + "*/" + objectType)
 		}
-		return routeHandler(activity)
+		return routeHandler(context, activity)
 	}
 
 	if routeHandler, ok := router.routes["*/*"]; ok {
 		if packageDebugLevel >= DebugLevelVerbose {
 			fmt.Println("HANNIBAL: Found Route: */*")
 		}
-		return routeHandler(activity)
+		return routeHandler(context, activity)
 	}
 
 	return derp.NewBadRequestError("hannibal.pub.Router.Handle", "No route found for activity", activity.Value())
