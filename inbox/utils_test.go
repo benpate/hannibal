e64d897910ac9ae49750d13f4a87e18a09d27222
Ben Pate <ben@pate.org>
2024-02-29 09:42:45 -0700

    Test Cases
    (mostly for the linter, tho)

---
+++ inbox/utils_test.go
@@ -0,0 +1,35 @@
+package inbox
+
+import (
+	"testing"
+
+	"github.com/rs/zerolog"
+	"github.com/stretchr/testify/require"
+)
+
+func TestCanTrace(t *testing.T) {
+	zerolog.SetGlobalLevel(zerolog.TraceLevel)
+	require.True(t, canTrace())
+	zerolog.SetGlobalLevel(zerolog.DebugLevel)
+	require.False(t, canTrace())
+}
+
+func TestCanDebug(t *testing.T) {
+	zerolog.SetGlobalLevel(zerolog.TraceLevel)
+	require.True(t, canDebug())
+	zerolog.SetGlobalLevel(zerolog.DebugLevel)
+	require.True(t, canDebug())
+	zerolog.SetGlobalLevel(zerolog.InfoLevel)
+	require.False(t, canDebug())
+}
+
+func TestCanInfo(t *testing.T) {
+	zerolog.SetGlobalLevel(zerolog.TraceLevel)
+	require.True(t, canDebug())
+	zerolog.SetGlobalLevel(zerolog.DebugLevel)
+	require.True(t, canDebug())
+	zerolog.SetGlobalLevel(zerolog.InfoLevel)
+	require.True(t, canInfo())
+	zerolog.SetGlobalLevel(zerolog.WarnLevel)
+	require.False(t, canInfo())
+}
