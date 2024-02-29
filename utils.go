595d68bb0a9a19f02601d7fe29b812d0ba4e1210
Ben Pate <ben@pate.org>
2024-02-29 09:08:44 -0700

    Add IsActivityPubContentType

--- utils.go
+++ utils.go
@@ -3,8 +3,26 @@ package hannibal
 import (
 	"net/http"
 	"time"
+
+	"github.com/benpate/hannibal/vocab"
 )
 
+// TimeFormat returns a string representation of the provided time value,
+// using the format designated by the W3C spec: https://www.w3.org/TR/activitystreams-core/#dates
 func TimeFormat(value time.Time) string {
 	return value.UTC().Format(http.TimeFormat)
+}
+
+// IsActivityPubContentType returns TRUE if the provided contentType is a valid ActivityPub content type.
+// https://www.w3.org/TR/activitystreams-core/#media-type
+func IsActivityPubContentType(contentType string) bool {
+
+	switch contentType {
+	case vocab.ContentTypeActivityPub,
+		vocab.ContentTypeJSONLDWithProfile:
+		return true
+
+	}
+
+	return false
 }
