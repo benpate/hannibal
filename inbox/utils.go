3b928feed110c787389271be06fdbff2f220fe8b
Ben Pate <ben@pate.org>
2024-08-29 13:28:43 -0600

    Remove unused code

--- inbox/utils.go
+++ inbox/utils.go
@@ -1,23 +1,8 @@
 package inbox
 
 import (
-	"github.com/benpate/hannibal/vocab"
 	"github.com/rs/zerolog"
 )
-
-// IsActivityPubContentType returns true if the specified content-type is an ActivityPub content-type
-func IsActivityPubContentType(contentType string) bool {
-
-	if contentType == vocab.ContentTypeActivityPub {
-		return true
-	}
-
-	if contentType == vocab.ContentTypeJSONLD {
-		return true
-	}
-
-	return false
-}
 
 // canLog is a silly zerolog helper that returns TRUE
 // if the provided log level would be allowed
