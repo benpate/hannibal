5d83ab03168ad265b7aa6c01f3166d9f2ed0268a
Ben Pate <ben@pate.org>
2025-01-01 14:32:24 -0700

    Add LinkTypeHashtag
    "finally"

--- vocab/linkTypes.go
+++ vocab/linkTypes.go
@@ -2,3 +2,8 @@ package vocab
 
 // https://www.w3.org/TR/activitystreams-vocabulary/#dfn-mention
 const LinkTypeMention = "Mention"
+
+// Discussed, but not defined in https://www.w3.org/TR/activitystreams-vocabulary/#h-microsyntaxes
+// https://socialhub.activitypub.rocks/t/formally-defining-a-hashtag-type/3429
+// Because "ActivityPub"...
+const LinkTypeHashtag = "Hashtag"
