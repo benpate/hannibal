ba6514b114609e46a22e177b51413cfe6f35197c
Ben Pate <ben@pate.org>
2026-05-28 06:58:54 -0600

    Comment

--- sigs/httpsig.go
+++ sigs/httpsig.go
@@ -1,3 +1,4 @@
 // Package sigs implements the IETF draft specification "Signing HTTP Messages"
 // https://datatracker.ietf.org/doc/html/draft-cavage-http-signatures
+// https://swicg.github.io/activitypub-http-signature/
 package sigs
