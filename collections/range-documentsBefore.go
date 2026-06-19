f001287389340d5333ae4f6d93a396c43d94ebbd
Ben Pate <ben@pate.org>
2026-06-18 20:23:10 -0600

    Bugfix: Prevent integer overflow
    The previous code could break if the limit was zero. This is a more explicit fix that corrects the problem.

--- collections/range-documentsBefore.go
+++ collections/range-documentsBefore.go
@@ -2,7 +2,6 @@ package collections
 
 import (
 	"iter"
-	"math"
 	"time"
 
 	"github.com/benpate/hannibal/streams"
@@ -12,10 +11,8 @@ func RangeDocumentsBefore(iterator iter.Seq[streams.Document], limit int64) iter
 
 	return func(yield func(streams.Document) bool) {
 
-		// Empty limit actually means "the end of time"
-		if limit == 0 {
-			limit = math.MaxInt64
-		}
+		// Determine if the limit is non-zero.
+		hasLimit := (limit > 0)
 
 		// Convert to a time.Time object for comparison
 		limitTime := time.Unix(limit, 0)
@@ -24,7 +21,7 @@ func RangeDocumentsBefore(iterator iter.Seq[streams.Document], limit int64) iter
 		for document := range iterator {
 
 			// Filter out documents that are equal or after the "before" date
-			if document.Published().After(limitTime) {
+			if hasLimit && document.Published().After(limitTime) {
 				continue
 			}
 
